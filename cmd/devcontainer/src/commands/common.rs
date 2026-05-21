74787f78c77da3229235ba6e836d2546a42780c2
Johan Carlin <johan.carlin@gmail.com>
2026-05-21 23:19:06 +0200

    Remove unused package collection re-export

--- cmd/devcontainer/src/commands/common.rs
+++ cmd/devcontainer/src/commands/common.rs
@@ -16,7 +16,7 @@ pub(crate) use config_resolution::{
     load_resolved_config, load_resolved_config_with_id_labels, resolve_override_config_path,
     resolve_read_configuration_path,
 };
-pub(crate) use fs::{copy_directory_recursive, package_collection_target};
+pub(crate) use fs::copy_directory_recursive;
 pub(crate) use labels::{
     default_devcontainer_id_label_pairs, default_devcontainer_id_labels,
     normalize_devcontainer_label_path, normalize_devcontainer_label_path_for_platform,
